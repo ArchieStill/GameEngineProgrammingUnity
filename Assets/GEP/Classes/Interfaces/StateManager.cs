0a73e703433f3b7ee6d5fdb12bfb50345d29f619
ArchieStill <archie2.still@live.uwe.ac.uk>
2023-11-13 12:04:44 +0000

    ui turns on and off and designed inventory base

--- Assets/GEP/Classes/Interfaces/StateManager.cs
+++ Assets/GEP/Classes/Interfaces/StateManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StateManager : MonoBehaviour
 {
     bool isPlayer;
     public bool IsPlayer { get =>  isPlayer; }
+
+    public GameObject UIBackground;
+    public GameObject UIBase;
+    public GameObject UITitle;
 
     void Start()
     {
@@ -15,5 +20,19 @@ public class StateManager : MonoBehaviour
     public void toggleIsPlayer()
     {
         isPlayer = !isPlayer;
+        if (isPlayer) 
+        {
+            UIBackground.SetActive(false);
+            UIBase.SetActive(false);
+            UITitle.SetActive(false);
+            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            UIBackground.SetActive(true);
+            UIBase.SetActive(true);
+            UITitle.SetActive(true);
+            UnityEngine.Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
