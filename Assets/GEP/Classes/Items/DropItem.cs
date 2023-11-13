0b21ca9c13bf1b539d0dcb656e333f8902f32688
ArchieStill <archie2.still@live.uwe.ac.uk>
2023-11-13 16:20:14 +0000

    continued dropitem and trying to instantiate in front of player

--- Assets/GEP/Classes/Items/DropItem.cs
+++ Assets/GEP/Classes/Items/DropItem.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DropItem : MonoBehaviour
 {
     public GameObject item;
+    public GameObject player;
+    private Vector3 facingDirection;
 
     public void dropItem()
     {
-        Instantiate(item, new Vector3(0, 1, 0), Quaternion.identity);
+        Instantiate(item, facingDirection, Quaternion.identity);
     }
+
+    /// start by getting direction player's facing, create vector that normalises that direction and add to position
+    /// https://docs.unity3d.com/Manual/InstantiatingPrefabs.html
 }
